2de0017dc651bc7610461f479ca7694f16fb7abd
Igna Dwi Utami <Igna.Utami@bsi.co.id>
2020-01-28 15:55:46 +0700

    ready to push

--- program8/Program.cs
+++ program8/Program.cs
@@ -1,12 +1,127 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json.Serialization;
+using System.Threading;
+using System.Threading.Tasks;
+using McMaster.Extensions.CommandLineUtils;
+using PuppeteerSharp;
+using System.Text;
 
-namespace program8
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace app7 {
+    class Program {
+        public static async Task<int> Main (string[] args) {
+            var ScreenShotprog = new CommandLineApplication () {
+                Name = "StringTransformation",
+                Description = "it should modify the string",
+                ShortVersionGetter = () => "1.0.0"
+            };
+
+            ScreenShotprog.Command ("screenshot", app => {
+                app.Description = "take a screenshot from web and save it in spesific format";
+                var input = app.Argument ("text", "Enter text");
+                var format = app.Option ("--format", "text length", CommandOptionType.SingleOrNoValue);
+                var filename = app.Option ("--output", "text length", CommandOptionType.SingleOrNoValue);
+                app.OnExecute (() => {
+                    string frm = "png";
+                    string url = Convert.ToString (input.Value);
+                    if (format.HasValue ()) {
+                        frm = Convert.ToString (format.Value ());
+                        Task a = ScreenShot (url, frm);
+                        a.Wait ();
+                    }
+                    if (filename.HasValue ()) {
+                        string flnm = Convert.ToString (filename.Value ());
+                        StringBuilder frmt = new StringBuilder();
+                        StringBuilder name = new StringBuilder();
+                        for(int i=flnm.Length-4;i<flnm.Length;i++) {
+                            frmt.Append(flnm[i]);
+                        }
+                        for(int i=0;i<flnm.Length-4;i++) {
+                            name.Append(flnm[i]);
+                        }
+                        string nm = Convert.ToString(name);
+                        string fm = Convert.ToString(frmt);
+                        Task a = fileName (url, nm, fm);
+                        a.Wait ();
+                    }
+                });
+            });
+
+            ScreenShotprog.OnExecute (() => {
+                ScreenShotprog.ShowHelp ();
+            });
+            return ScreenShotprog.Execute (args);
+        }
+
+        public static async Task ScreenShot (string url, string format) {
+            await new BrowserFetcher ().DownloadAsync (BrowserFetcher.DefaultRevision);
+            var browser = await Puppeteer.LaunchAsync (new LaunchOptions {
+                Headless = false
+            });
+            var page = await browser.NewPageAsync ();
+            await page.GoToAsync (url);
+            var optionScreen = new ScreenshotOptions () {
+                FullPage = true
+            };
+
+            if (format == "pdf") {
+                string path = "D:/TRAININGC#/JANUARI/23Januari APP CMD/Task/program8";
+                string[] getFile = Directory.GetFiles (path);
+                foreach (string item in getFile) {
+                    int counter = 1;
+                    string file_name = Path.GetFileNameWithoutExtension (item);
+                    string final = file_name.Replace ("screenshot-", "");
+                    if (final == Convert.ToString (counter)) {
+                        counter++;
+                        await page.ScreenshotAsync ("screenshot-" + counter + ".pdf");
+                    } else {
+                        await page.ScreenshotAsync ("screenshot-" + counter + ".pdf");
+                    }
+                }
+            } else if (format == "jpg") {
+                string loc = "D:/TRAININGC#/JANUARI/23Januari APP CMD/Task/program8";
+                string[] getFile = Directory.GetFiles (loc);
+                foreach (string item in getFile) {
+                    int count = 1;
+                    string file_name = Path.GetFileNameWithoutExtension (item);
+                    string final = file_name.Replace ("screenshot-", "");
+                    if (final == Convert.ToString (count)) {
+                        count++;
+                        await page.ScreenshotAsync ("screenshot-" + count + ".jpg");
+                    } else {
+                        await page.ScreenshotAsync ("screenshot-" + count + ".jpg");
+                    }
+                }
+            } else {
+                string path = "D:/TRAININGC#/JANUARI/23Januari APP CMD/Task/program8";
+                string[] getFile = Directory.GetFiles (path);
+                foreach (string item in getFile) {
+                    int count = 1;
+                    string file_name = Path.GetFileNameWithoutExtension (item);
+                    string final = file_name.Replace ("screenshot-", "");
+                    if (final == Convert.ToString (count)) {
+                        count++;
+                        await page.ScreenshotAsync ("screenshot-" + count + ".png");
+                    } else {
+                        await page.ScreenshotAsync ("screenshot-" + count + ".png");
+                    }
+                }
+            }
+        }
+
+        public static async Task fileName (string url, string nama, string format) {
+            await new BrowserFetcher ().DownloadAsync (BrowserFetcher.DefaultRevision);
+            var browser = await Puppeteer.LaunchAsync (new LaunchOptions {
+                Headless = true
+            });
+            var page = await browser.NewPageAsync ();
+            await page.GoToAsync (url);
+            var optionScreen = new ScreenshotOptions () {
+                FullPage = true
+            };
+            await page.ScreenshotAsync ($"{nama}{format}");
+            await browser.CloseAsync ();
         }
     }
 }
--- program9/Program.cs
+++ program9/Program.cs
@@ -1,13 +1,81 @@
 ﻿using System;
 using McMaster.Extensions.CommandLineUtils;
+using PuppeteerSharp;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
 
-namespace program9
+namespace soal9
 {
     class Program
     {
-        static void Main(string[] args)
+          public static async Task screenshot(string url ,string format)
+          {
+            await new BrowserFetcher().DownloadAsync(BrowserFetcher.DefaultRevision);
+            var browser = await Puppeteer.LaunchAsync(new LaunchOptions
+            {
+                Headless = true
+            });
+            var page = await browser.NewPageAsync();
+            await page.GoToAsync(url);
+
+            var screenshot_option = new ScreenshotOptions()
+            {
+                FullPage = true
+            };
+
+            if(format == "jpg" || format == "png")
+            {
+                if(format == "jpg")
+                {
+                    string repl = url.Replace("/","_").Replace(":","_");
+                    await page.ScreenshotAsync($"{repl}.{format}",screenshot_option);
+                    await page.CloseAsync();
+                }
+                else if (format == "png")
+                {
+                    string repl = url.Replace("/","_").Replace(":","_");
+                    await page.ScreenshotAsync($"{repl}.{format}",screenshot_option);
+                    await page.CloseAsync();
+                }
+            }
+            else if(format =="pdf")
+            {
+                string repl = url.Replace("/","_").Replace(":","_");
+                await page.PdfAsync($"{repl}.{format}");
+                await page.CloseAsync();
+            }
+        }
+        static async Task<int> Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var Sc2 = new CommandLineApplication()
+            {
+                Name = "get screenshot2",
+                Description = "get screenshot from list file",
+                ShortVersionGetter = () => "1.0.0",
+            };
+
+            Sc2.Command("screenshot-list",app => 
+            {
+                app.Description = "get screenshot from list file";
+
+                var input = app.Argument("Text","Masukkan Text");
+                var format = app.Option("--format","formatfile",CommandOptionType.SingleOrNoValue);
+                
+                app.OnExecuteAsync(async cancellationToken => 
+                {
+                    if(format.HasValue())
+                    {
+                        var fl = File.ReadLines(input.Value);
+                        foreach(var item in fl)
+                        {
+                            await screenshot(item,format.Value());
+                        }
+                    }
+                });
+            });
+
+            return Sc2.Execute(args);
         }
     }
 }
